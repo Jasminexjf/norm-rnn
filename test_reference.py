747158fd9f945577e3aca07a98b5987b0563d53c
Gabriel Pereyra <gabriel@nervanasys.com>
2015-08-22 12:20:40 -0700

    working reference model

--- test_reference.py
+++ test_reference.py
@@ -4,10 +4,12 @@ np.random.seed(0)
 from datasets import PennTreebank
 from norm_rnn import List
 from norm_rnn import compile_model
+from utils import ProgressBar
 from layers import *
 
 # load data
 train_set = PennTreebank()
+valid_set = PennTreebank(path=PennTreebank.valid_path, vocab=train_set.vocab)
 
 # config model
 model = List([
@@ -17,13 +19,25 @@ model = List([
 ])
 
 # compile theano functions
-fit = compile_model(model, train_set)
+fit = compile_model(model, train_set, update=True)
+val = compile_model(model, valid_set, update=False)
 
 # train
-for epoch in range(1, 11):
-    print 'Epoch:', epoch
+for epoch in range(1, 10):
+    print 'epoch({})'.format(epoch)
 
     # fit
-    cost_list = []
-    for batch in train_set:
-        print fit(batch)
+    train_errors = []
+    train_progress = ProgressBar(range(len(train_set)))
+    for batch in train_progress:
+        error = fit(batch)
+        train_errors.append(error)
+        train_progress.cost = np.mean(train_errors)
+
+    # validate
+    valid_errors = []
+    valid_progress = ProgressBar(range(len(valid_set)))
+    for batch in valid_progress:
+        error = val(batch)
+        valid_errors.append(error)
+        valid_progress.cost = np.mean(valid_errors)
