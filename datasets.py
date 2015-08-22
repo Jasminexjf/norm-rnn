43c4b0b25fc4dceea6ea4e6a1df103ac5e1cf7e8
Gabriel Pereyra <gabriel@nervanasys.com>
2015-08-22 12:19:42 -0700

    moved progress bar to utils

--- datasets.py
+++ datasets.py
@@ -43,10 +43,6 @@ class PennTreebank(object):
         self.batch_size = batch_size
         self.time_steps = time_steps
 
-    def __iter__(self):
-        for batch in range(len(self)):
-            yield batch
-
     def __len__(self):
         # number of batches
         return len(self.X)
@@ -58,38 +54,3 @@ class PennTreebank(object):
     @property
     def index_to_token(self):
         return dict((v, k) for k, v in self.token_to_index.iteritems())
-
-
-class ProgressBar(object):
-
-    # http://code.activestate.com/recipes/576986-progress-bar-for-console-programs-as-iterator/
-
-    def __init__(self, iter, size=60):
-        self.iter = iter
-        self.size = size
-        self.loss = 0
-
-    def __iter__(self):
-        start = timeit.default_timer()
-        count = len(self.iter)
-
-        def _show(_i):
-            x = int(self.size * _i / count)
-            progress = '#' * x
-            remaining = '.' * (self.size - x)
-            time_elapsed = timeit.default_timer() - start
-            bar = '[{}{}] batch({}/{}) loss({:.2f}), time({:.2f})\r'.format(
-                progress, remaining, _i, count, self.loss, time_elapsed)
-            sys.stdout.write(bar)
-            sys.stdout.flush()
-
-        _show(0)
-
-        for i, item in enumerate(self.iter, 1):
-            yield item
-            _show(i)
-        sys.stdout.write("\n")
-        sys.stdout.flush()
-
-    def set_loss(self, loss):
-        self.loss = loss
