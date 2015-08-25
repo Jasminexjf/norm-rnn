6c48d7f9ed31598d50443b2635b55d57c7472623
Gabriel Pereyra <gabriel@nervanasys.com>
2015-08-25 08:55:03 -0700

    batch normalized lstm layer

--- layers.py
+++ layers.py
@@ -136,53 +136,18 @@ class LSTM(object):
 
 class BN(object):
 
-    def __init__(self, input_size, batch_size=None, time_steps=None, momentum=0.9, epsilon=1e-6):
+    def __init__(self, input_size, epsilon=1e-6):
         self.gamma = theano.shared(np.ones(input_size, dtype=np.float32))
         self.beta = theano.shared(np.zeros(input_size, dtype=np.float32))
         self.params = [self.gamma, self.beta]
 
-        # if dimensions are None, we normalize over them
-        # the resuling mean shape is all dimensions that were specified
-        # (e.g. to get standard batch-norm on a sequence, we need to
-        #  specify time-steps since our shared running averages needs this
-        #  to be initialized.)
-        running_shape = ()
-        self.axes = ()
-        if batch_size is not None:
-            running_shape += (batch_size, )
-        else:
-            self.axes += (0, )
-
-        if time_steps is not None:
-            running_shape += (time_steps, )
-        else:
-            self.axes += (1, )
-
-        running_shape += (input_size, )
-
-        self.running_shape = running_shape
-        self.reset_state()
-
-        self.momentum = momentum
         self.epsilon = epsilon
 
     def __call__(self, x):
-        # mini-batch statistics
-        m = x.mean(axis=self.axes)
-        std = std = T.mean((x - m) ** 2 + self.epsilon, axis=self.axes) ** 0.5
-
-        # updates for shared state
-        mean_update = self.momentum * self.running_mean + (1-self.momentum) * m
-        std_update = self.momentum * self.running_std + (1-self.momentum) * std
-        self.updates = [(self.running_mean, mean_update), (self.running_std, std_update)]
-
-        # normalize
-        x = (x - self.running_mean) / (self.running_std + self.epsilon)
+        m = x.mean(axis=0)
+        std = std = T.mean((x - m) ** 2 + self.epsilon, axis=0) ** 0.5
+        x = (x - m) / (std + self.epsilon)
         return self.gamma * x + self.beta
-
-    def reset_state(self):
-        self.running_mean = theano.shared(np.zeros(self.running_shape, np.float32))
-        self.running_std = theano.shared(np.zeros(self.running_shape, np.float32))
 
 
 class Dropout(object):
@@ -194,3 +159,51 @@ class Dropout(object):
 
     def __call__(self, x):
             return x * self.srng.binomial(x.shape, p=1-self.p, dtype=theano.config.floatX)
+
+
+class BNLSTM(LSTM):
+
+    def __init__(self, input_size, layer_size, activation=T.tanh,
+                 inner_activation=T.nnet.sigmoid, weight_init=Uniform()):
+        super(BNLSTM, self).__init__(input_size, layer_size, activation,
+                 inner_activation, weight_init)
+
+        # remove biases
+        self.params.pop(2)
+        self.params.pop(4)
+        self.params.pop(6)
+        self.params.pop(8)
+
+        # add batch norm layers
+        self.norm_xi = BN(layer_size)
+        self.norm_xf = BN(layer_size)
+        self.norm_xc = BN(layer_size)
+        self.norm_xo = BN(layer_size)
+
+        self.params.extend(self.norm_xi.params + self.norm_xf.params +
+                           self.norm_xc.params + self.norm_xo.params)
+
+    def __call__(self, x):
+        xi = self.norm_xi(T.dot(x, self.W_i))
+        xf = self.norm_xf(T.dot(x, self.W_f))
+        xc = self.norm_xc(T.dot(x, self.W_c))
+        xo = self.norm_xo(T.dot(x, self.W_o))
+
+        # (time_steps, batch_size, layer_size)
+        x = x.dimshuffle((1, 0, 2))
+
+        # if set_state hasn't been called, use temporary zeros (stateless)
+        if not self.shared_state:
+            self.h = self._alloc_zeros_matrix(x.shape[1], xi.shape[2])
+            self.c = self._alloc_zeros_matrix(x.shape[1], xi.shape[2])
+
+        [outputs, memories], updates = theano.scan(self._step,
+            sequences=[xi, xf, xo, xc],
+            outputs_info=[self.h, self.c],
+            non_sequences=[self.U_i, self.U_f, self.U_o, self.U_c],
+        )
+
+        if self.shared_state:
+            self.updates = [(self.h, outputs[-1]), (self.c, memories[-1])]
+
+        return outputs.dimshuffle((1, 0, 2))
