93c7af6b10fed794cca28ee685af728b26ad909e
Gabriel Pereyra <gabriel@nervanasys.com>
2015-08-26 07:55:22 -0700

    cleaned up reference models

--- reference/large.py
+++ reference/large.py
@@ -1,95 +1,157 @@
+import sys
 import numpy as np
 np.random.seed(0)
 
-from datasets import PennTreebank
+# lasagne loader (merge into datasets)
+from tests.test_dataset import LasagneLoader
 from utils import ProgressBar
 from norm_rnn import *
 from layers import *
 
-# hyper parameters
-layer_size = 1500
-time_steps = 35
-batch_size = 20
-learning_rate = 1
-decay_rate = 0.5
-max_norm = 10
-decay_epoch = 14
-epochs = 55
-drop_prob = 0.65
-init_range = 0.04
+# try to import plotly
+try:
+    import plotly.plotly as py
+    from plotly.graph_objs import *
+    plotly_is_installed = True
+except ImportError:
+    print 'Run \'pip install plotly\' to get a dope plot after training'
+    plotly_is_installed = False
 
-# load data
-train_set = PennTreebank(batch_size, time_steps)
-valid_set = PennTreebank(1, time_steps,
-                         PennTreebank.valid_path, train_set.vocab)
+# models params
+init_range = 0.04
+layer_size = 1500
+drop_prob = 0.65
+
+# data params
+time_steps = 32
+batch_size = 20
+epochs = 55
+
+# optimizer params
+learning_rate = 1
+decay_rate = 1 / 1.15
+decay_epoch = 14
+max_norm = 10
+
+# load dataset
+dataset = LasagneLoader()
+vocab_size = len(dataset.vocab_map)
+train_batches = len(dataset.X_train) / batch_size
+valid_batches = len(dataset.X_valid) / batch_size
+test_batches = len(dataset.X_test) / batch_size
 
 # weight init
 weight_init = Uniform(init_range)
 
 # config model
+model_name = 'Large Reference'
 model = List([
-    Embed(len(train_set.vocab), layer_size, weight_init=weight_init),
+    Embed(vocab_size, layer_size, weight_init=weight_init),
     Dropout(drop_prob),
     LSTM(layer_size, layer_size, weight_init=weight_init),
     Dropout(drop_prob),
     LSTM(layer_size, layer_size, weight_init=weight_init),
-    Linear(layer_size, len(train_set.vocab), activation=Identity(), weight_init=weight_init),
+    Linear(layer_size, vocab_size, Identity(), weight_init=weight_init),
     Dropout(drop_prob),
     Softmax(),
 ])
 
-# initialize shared memory for lstm states
-# (need to find a way to push this into the layer)
+# set LSTM states
 for layer in model.layers:
     if isinstance(layer, LSTM):
         layer.set_state(batch_size)
 
-# initialize optimizer
-grad_norm = MaxNorm(max_norm)
-decay = DecayEvery(decay_epoch * len(train_set), decay_rate)
-optimizer = SGD(learning_rate, grad_norm, decay)
+# optimizer
+clip = Clip()
+decay = DecayEvery(decay_epoch * train_batches, decay_rate)
+optimizer = RMS(grad=clip, decay=decay)
 
-# compile theano functions
-fit = compile_model(model, train_set, optimizer)
+# compile
+fit = compile_model_lasagne(model, (dataset.X_train, dataset.y_train), optimizer)
 
-# strip out drop before compiling valid function (so lazy)
-def not_dropout(layer): return not isinstance(layer, Dropout)
-model.layers = filter(not_dropout, model.layers)
+# remove dropout layers
+model.layers.pop(1)
+model.layers.pop(2)
+model.layers.pop(4)
 
-val = compile_model(model, valid_set)
+val = compile_model_lasagne(model, (dataset.X_valid, dataset.y_valid))
 
-# train
-for epoch in range(1, epochs + 1):
-    print 'epoch({})'.format(epoch)
 
-    # fit
+# plotly data
+data = []
+colors = ['rgb(31, 119, 180)',
+          'rgb(255, 127, 14)']
+
+
+# epoch helper (move to model)
+def apply_to_batches(f, batches):
     perplexity_list = []
     accuracy_list = []
-    train_progress = ProgressBar(range(len(train_set)))
-    for batch in train_progress:
-        perplexity, accuracy = fit(batch)
+    progress = ProgressBar(range(batches))
+    for batch in progress:
+        perplexity, accuracy = f(batch)
         perplexity_list.append(perplexity)
         accuracy_list.append(accuracy)
-        train_progress.perplexity = np.mean(perplexity_list)
-        train_progress.accuracy = np.mean(accuracy_list)
+        progress.perplexity = np.mean(perplexity_list)
+        progress.accuracy = np.mean(accuracy_list)
 
-    # reset lstm layers
-    for layer in model.layers:
-        if isinstance(layer, LSTM):
-            layer.set_state(1)
-
-    # validate
-    perplexity_list = []
-    accuracy_list = []
-    valid_progress = ProgressBar(range(len(valid_set)))
-    for batch in valid_progress:
-        perplexity, accuracy = val(batch)
-        perplexity_list.append(perplexity)
-        accuracy_list.append(accuracy)
-        valid_progress.perplexity = np.mean(perplexity_list)
-        valid_progress.accuracy = np.mean(accuracy_list)
-
-    # reset lstm layers
+    # reset LSTM layer
     for layer in model.layers:
         if isinstance(layer, LSTM):
             layer.set_state(batch_size)
+
+    return np.mean(perplexity_list)
+
+print
+print 'model({})'.format(model_name)
+print
+
+# train
+train_costs = []
+valid_costs = []
+for epoch in range(1, epochs + 1):
+    print 'epoch({})'.format(epoch)
+    train_costs.append(apply_to_batches(fit, train_batches))
+    valid_costs.append(apply_to_batches(val, valid_batches))
+
+# static test validation
+print 'test type(static)'
+val = compile_model_lasagne(model, (dataset.X_test, dataset.y_test))
+static_cost = apply_to_batches(val, test_batches)
+
+# dynamic test validation
+print 'test type(dynamic)'
+fit = compile_model_lasagne(model, (dataset.X_test, dataset.y_test), optimizer)
+dynamic_cost = apply_to_batches(fit, test_batches)
+
+if plotly_is_installed:
+    color = colors.pop()
+
+    # plot train results
+    train_trace = Scatter(x=range(len(train_costs)),
+                     y=train_costs,
+                     name='{} (Train))'.format(model_name),
+                     color=Line(color=color))
+
+    # plot valid results
+    valid_trace = Scatter(x=range(len(valid_costs)),
+                     y=valid_costs,
+                     name='{} (Valid))'.format(model_name),
+                     line=Line(color=color, dash='dot'))
+
+    # collect traces
+    data.append(train_trace)
+    data.append(valid_trace)
+
+
+if plotly_is_installed:
+    # x and y labels
+    layout = Layout(xaxis=XAxis(title='Epochs'),
+                    yaxis=YAxis(title='Perplexity'))
+
+    # save plot and open in browser
+    fig = Figure(data=Data(data), layout=layout)
+    py.image.save_as(fig, '{}.png'.format(sys.modules[__name__].__name__))
+    plot_url = py.plot(data, filename='basic-line')
+else:
+    print 'No plot for you!'
--- reference/small.py
+++ reference/small.py
@@ -6,6 +6,9 @@ from utils import ProgressBar
 from norm_rnn import *
 from layers import *
 
+# lasagne loader for debugging
+from tests.test_dataset import LasagneLoader
+
 # hyper parameters
 layer_size = 200
 time_steps = 20
@@ -13,86 +16,104 @@ batch_size = 20
 max_norm = 5
 decay_rate = 0.5
 decay_epoch = 4
-learning_rate = 1
-epochs = 6
+learning_rate = 0.001
+epochs = 5
 
-# load data
-train_set = PennTreebank(batch_size, time_steps)
-valid_set = PennTreebank(batch_size, time_steps,
-                         PennTreebank.valid_path, train_set.vocab)
-test_set = PennTreebank(2, time_steps,
-			PennTreebank.test_path, train_set.vocab)
+# load dataset
+dataset = LasagneLoader()
+vocab_size = len(dataset.vocab_map)
+train_batches = len(dataset.X_train) / batch_size
+valid_batches = len(dataset.X_valid) / batch_size
+test_batches = len(dataset.X_test) / batch_size
+
+# to test plotly
+train_batches = valid_batches = test_batches = 10
 
 # config model
+model_name = 'Reference'
 model = List([
-    Embed(len(train_set.vocab), layer_size),
+    Embed(vocab_size, layer_size),
     LSTM(layer_size, layer_size),
     LSTM(layer_size, layer_size),
-    Linear(layer_size, len(train_set.vocab))
+    Linear(layer_size, vocab_size)
 ])
 
 # initialize shared memory for lstm states
 # (find a way to push this into the layer)
 for layer in model.layers:
     if isinstance(layer, LSTM):
-        layer.set_state(train_set.batch_size)
+        layer.set_state(batch_size)
 
 # initialize optimizer
-grad_norm = MaxNorm(max_norm)
-decay = DecayEvery(decay_epoch * len(train_set), decay_rate)
-optimizer = SGD(learning_rate, grad_norm, decay)
+clip = Clip()
+decay = DecayEvery(decay_epoch * train_batches, decay_rate)
+optimizer = RMS(grad=clip, decay=decay)
 
 # compile theano functions
-fit = compile_model(model, train_set, optimizer)
-val = compile_model(model, valid_set)
+fit = compile_model_lasagne(model, (dataset.X_train, dataset.y_train), optimizer)
+val = compile_model_lasagne(model, (dataset.X_valid, dataset.y_valid))
 
-# train
-for epoch in range(1, epochs + 1):
-    print 'epoch({})'.format(epoch)
 
-    # fit
+# function to compute epoch and return loss
+# (put this in model?)
+def apply_to_batches(f, batches):
     perplexity_list = []
     accuracy_list = []
-    train_progress = ProgressBar(range(len(train_set)))
+    train_progress = ProgressBar(range(batches))
     for batch in train_progress:
-        perplexity, accuracy = fit(batch)
+        perplexity, accuracy = f(batch)
         perplexity_list.append(perplexity)
         accuracy_list.append(accuracy)
         train_progress.perplexity = np.mean(perplexity_list)
         train_progress.accuracy = np.mean(accuracy_list)
 
-    # validate
-    perplexity_list = []
-    accuracy_list = []
-    valid_progress = ProgressBar(range(len(valid_set)))
-    for batch in valid_progress:
-        perplexity, accuracy = val(batch)
-        perplexity_list.append(perplexity)
-        accuracy_list.append(accuracy)
-        valid_progress.perplexity = np.mean(perplexity_list)
-        valid_progress.accuracy = np.mean(accuracy_list)
-
-    # reset lstm layers
+    # reset LSTM layer
     for layer in model.layers:
         if isinstance(layer, LSTM):
-            layer.set_state(train_set.batch_size)
+            layer.set_state(batch_size)
+
+    return np.mean(perplexity_list)
+
+# train
+train_costs = []
+valid_costs = []
+for epoch in range(1, epochs + 1):
+    print 'epoch({})'.format(epoch)
+    train_costs.append(apply_to_batches(fit, train_batches))
+    valid_costs.append(apply_to_batches(val, valid_batches))
 
 
-# reset lstm layers
-for layer in model.layers:
-    if isinstance(layer, LSTM):
-        layer.set_state(2)
+print range(len(train_costs))
+print train_costs
 
-val = compile_model(model, test_set)
+print 'test type(static)'
+val = compile_model_lasagne(model, (dataset.X_test, dataset.y_test))
+static_cost = apply_to_batches(val, test_batches)
 
-# validate
-perplexity_list = []
-accuracy_list = []
-valid_progress = ProgressBar(range(len(test_set)))
-for batch in valid_progress:
-    perplexity, accuracy = val(batch)
-    perplexity_list.append(perplexity)
-    accuracy_list.append(accuracy)
-    valid_progress.perplexity = np.mean(perplexity_list)
-    valid_progress.accuracy = np.mean(accuracy_list)
 
+print 'test type(dynamic)'
+fit = compile_model_lasagne(model, (dataset.X_test, dataset.y_test), optimizer)
+dynamic_cost = apply_to_batches(fit, test_batches)
+
+
+# plot results
+import sys
+import plotly.plotly as py
+from plotly.graph_objs import *
+
+trace0 = Scatter(x=range(len(train_costs)),
+                 y=train_costs,
+                 name='{} (Train))'.format(model_name))
+
+trace1 = Scatter(x=range(len(valid_costs)),
+                 y=valid_costs,
+                 name='{} (Valid))'.format(model_name),
+                 line=Line(dash='dot'))
+
+layout = Layout(xaxis=XAxis(title='Epochs'),
+                yaxis=YAxis(title='Perplexity'))
+
+data = Data([trace0, trace1])
+fig = Figure(data=data, layout=layout)
+py.image.save_as(fig, '{}.png'.format(sys.modules[__name__].__name__))
+plot_url = py.plot(data, filename='basic-line')
