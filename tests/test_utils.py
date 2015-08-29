b27ba965f876355016fb6115df9245c48008c895
Gabriel Pereyra <gabriel@nervanasys.com>
2015-08-29 16:00:57 -0700

    fix utils test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,6 @@
 def test_progress_bar():
     from utils import TrainProgressBar
-    progress_bar = TrainProgressBar(10, 10)
+    progress_bar = TrainProgressBar(1, 10, 10)
     progress_bar.fit_update(zip(range(10), range(10)))
     progress_bar.val_update(zip(range(5), range(5)))
 
