dbf0f5a3fb0e07d6d6880d40c100bc6b6a7fd2cc
Gabriel Pereyra <gabriel@nervanasys.com>
2015-09-01 21:19:03 -0700

    fix large ref model

--- experiments/ptb_large_norm.py
+++ experiments/ptb_large_norm.py
@@ -1,6 +1,5 @@
-from ptb_large_ref import (vocab_size, layer_size,
-                           optimizer, dataset, epochs,
-                           drop_prob, weight_init)
+from ptb_medium_ref import (vocab_size, layer_size, drop_prob, weight_init,
+                           optimizer, train_set, valid_set, test_set, epochs)
 from model import List
 from layers import *
 
@@ -8,18 +7,13 @@ from layers import *
 model = List([
     Embed(vocab_size, layer_size, weight_init=weight_init),
     Dropout(drop_prob),
-    LSTM(layer_size, layer_size, weight_init=weight_init),
+    BNLSTM(layer_size, layer_size, weight_init=weight_init),
     Dropout(drop_prob),
-    LSTM(layer_size, layer_size, weight_init=weight_init),
+    BNLSTM(layer_size, layer_size, weight_init=weight_init),
     Dropout(drop_prob),
     Linear(layer_size, vocab_size, weight_init=weight_init)])
 
 
 if __name__ == '__main__':
-    # compile
-    model.compile(dataset, optimizer)
-    model.compile(dataset)
-
-    # train
-    model.train(epochs)
-    model.dump('ptb_large_norm_results.pkl')
+    model.train(train_set, valid_set, test_set, optimizer, epochs)
+    model.dump('ptb_medium_norm_results.pkl')
--- experiments/ptb_large_ref.py
+++ experiments/ptb_large_ref.py
@@ -1,30 +1,34 @@
 import numpy as np
 np.random.seed(0)
 
-from datasets import PennTreebank
+from datasets import *
 from optimizers import *
 from model import *
 from layers import *
 
-# models params
-init_range = 0.04
-layer_size = 1500
-drop_prob = 0.65
-
 # data params
 time_steps = 35
-batch_size = 20
-epochs = 50
+batch_size = 32
+
+# models params
+layer_size = 1500
+drop_prob = 0.65
+init_range = 0.04
 
 # optimizer params
 learning_rate = 1
 decay_rate = 1 / 1.15
 decay_epoch = 14
 max_norm = 10
+epochs = 60
 
 # data
-dataset = PennTreebank(batch_size, time_steps)
-vocab_size = len(dataset.vocab_map)
+train_set = Text(ptb_train_path, batch_size, time_steps)
+valid_set = Text(ptb_valid_path, batch_size, time_steps,
+                 vocab_map=train_set.vocab_map, vocab_index=train_set.vocab_idx)
+test_set = Text(ptb_test_path, batch_size, time_steps,
+                 vocab_map=train_set.vocab_map, vocab_index=train_set.vocab_idx)
+vocab_size = len(train_set.vocab_map)
 
 # weight init
 weight_init = Uniform(init_range)
@@ -37,19 +41,15 @@ model = List([
     Dropout(drop_prob),
     LSTM(layer_size, layer_size, weight_init=weight_init),
     Dropout(drop_prob),
-    Linear(layer_size, vocab_size, weight_init=weight_init)])
+    Linear(layer_size, vocab_size, weight_init=weight_init)
+])
 
 # optimizer
-grad = MaxNorm()
-decay = DecayEvery(decay_epoch * len(dataset.X_train.get_value()), decay_rate)
+grad = MaxNorm(max_norm)
+decay = DecayEvery(decay_epoch * train_set.batches, decay_rate)
 optimizer = SGD(learning_rate, grad, decay)
 
 
 if __name__ == '__main__':
-    # compile
-    model.compile(dataset, optimizer)
-    model.compile(dataset)
-
-    # train
-    model.train(epochs)
-    model.dump('ptb_large_ref_results.pkl')
+    model.train(train_set, valid_set, test_set, optimizer, epochs)
+    model.dump('ptb_medium_ref_results.pkl')
